402ebd522af5d1dcbed6999eb5dc092f6eae17b6
Ravindra Joisa <ravindra.joisa@hotmail.com>
2019-03-04 23:09:35 +0530

    Datatypes
    Training on Java DataTypes

---
+++ JavaTraining/src/javaBasics/DataTypes.java
@@ -0,0 +1,39 @@
+package javaBasics;
+
+/**
+ * @author ravindra
+ *
+ */
+public class DataTypes {
+
+	public static void main(String[] args) {
+		
+		//Primitive data types: int, double, char, boolean
+		//int
+		int i = 10;
+		System.out.println(i);
+		i = 20;
+		System.out.println(i);
+		
+		//double
+		double d = 23.44;
+		System.out.println(d);
+		System.out.println(i+d);
+		
+		//char
+		char c = 'a';
+		char c1 = '$';
+		System.out.println(c);
+		System.out.println(c1);
+		
+		//boolean
+		boolean b1 = true;
+		boolean b2 = false;
+		System.out.println(b1);
+		
+		//String - Not a data type, but can be used. It is a class.
+		String s = "Hello Man";
+		System.out.println(s);
+	}
+
+}
