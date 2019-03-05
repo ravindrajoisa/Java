daa620ce47248bca91c7e0575f3d6a083002cbcc
Ravindra Joisa <ravindra.joisa@hotmail.com>
2019-03-05 15:51:41 +0530

    package renamed

---
+++ JavaTraining/src/oopConcepts/MethodsInJava.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package oopConcepts;
+
+/**
+ * @author ravin
+ *
+ */
+public class MethodsInJava {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
