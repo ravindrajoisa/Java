265c1731f0c40135fc08cf60f2a3cfc38858c7b8
Ravindra Joisa <ravindra.joisa@hotmail.com>
2019-03-07 16:03:09 +0530

    Singleton in Java

---
+++ JavaTraining/src/javaBasics/Singleton.java
@@ -0,0 +1,55 @@
+/**
+ * 
+ */
+package javaBasics;
+
+/**
+ * @author ravindra
+ *
+ */
+public class Singleton {
+
+	/**
+	 * Singleton - we can have only one object, we can instantiate only one object.
+	 * On trying to create a new object with different reference, it will point to the same object. 
+	 * Design of singleton class:
+	 * 1. make a private constructor
+	 * 2. Lazy initialization - write a public static method that has a return type of object of this singleton class.
+	 * Singleton patterns are used in logging, caches, thread pools, configuration settings, device driver objects.
+	 */
+	
+	private Singleton() {
+		str = "ravindra";
+	}
+	
+	private static Singleton s = null;
+		String str;
+	
+	public static Singleton getInstance() {			//lazy initialization
+		if (s == null)
+			s = new Singleton();
+			return s;
+		}
+	
+	public static void main(String[] args) {
+		
+		Singleton x = Singleton.getInstance();
+		Singleton y = Singleton.getInstance();
+		Singleton z = Singleton.getInstance();
+		
+		x.str = (x.str).toUpperCase();
+		
+		System.out.println(x.str);
+		System.out.println(y.str);
+		System.out.println(z.str);
+		
+		System.out.println("******");
+		
+		z.str = (z.str).toLowerCase();
+		
+		System.out.println(x.str);
+		System.out.println(y.str);
+		System.out.println(z.str);
+	}
+
+}
